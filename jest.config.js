8b84745475d9e7fe385c137b47487a3353fcf05d
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-07-08 20:28:07 -0300

    refactor: jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnviroment: 'node',
